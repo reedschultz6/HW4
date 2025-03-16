9237b92178990f20fa338477f2fd1fe702617772
Reed Schultz <reed.schultz@kellogg.northwestern.edu>
2025-03-16 18:15:33 +0000

    hopefully fixed image problem

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,5 @@
 class EntriesController < ApplicationController
+
   def index
     @entries = Entry.where({ "user_id" => session["user_id"] })
   end
@@ -6,21 +7,20 @@ class EntriesController < ApplicationController
   def new
     @entry = Entry.new
   end
-  def new
-  end
 
   def create
     if User.find_by({ "id" => session["user_id"] }) != nil
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-else 
-      flash["notice"] = "Please login First."
-end 
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+    else 
+      flash["notice"] = "Login first."
+    end 
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
 end
